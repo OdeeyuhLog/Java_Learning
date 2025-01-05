cb6d87802463ebff0aaf07303ff56f11895fc601
Reapur <charlesfreud405@protonmail.com>
2025-01-05 09:19:24 +0800

    Project: Initialize shapes-calculator project

---
+++ Intermediate/shapes-calculator/src/main/java/com/intermediate/calculator/App.java
@@ -0,0 +1,13 @@
+package com.intermediate.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
