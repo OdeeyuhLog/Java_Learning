e50c5f862c430c0e0921710bf274878c50605131
Reapur <charlesfreud405@protonmail.com>
2025-01-07 14:37:30 +0800

    Set up java fx and init dependencies

--- Intermediate/text-edtior-app/src/main/java/com/intermediate/editor/App.java
+++ Intermediate/text-edtior-app/src/main/java/com/intermediate/editor/App.java
@@ -1,13 +1,21 @@
 package com.intermediate.editor;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Label label = new Label("Hello, everyone");
+    Scene scene = new Scene(label, 400, 200);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("JavaFx App");
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
